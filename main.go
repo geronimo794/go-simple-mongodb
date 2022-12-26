a702a9e7e23b9ad342d7b95689992641bef36595
Ach Rozikin@MacBook <geronimo794@gmail.com>
2022-12-26 23:25:45 +0700

    feat: Read update delete

--- main.go
+++ main.go
@@ -6,11 +6,14 @@ import (
 	"os"
 
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type Data struct {
+	ID   primitive.ObjectID `bson:"_id"`
 	Name string
 }
 
@@ -40,15 +43,18 @@ func main() {
 	/**
 	* Create mongo db connection by connection string
 	**/
-	// Context to pass the data
+	// Context to controll the process
 	ctx := context.TODO()
+	// ctx, cancel := context.WithTimeout(ctx, 10*time.Second) / If you are using context with timeout
 	// Connect to the mongodb
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoConnectionString))
 	if err != nil {
 		panic(err)
 	}
+
 	// Defer function to disconnect to the mongo db
 	defer func() {
+		// cancel() // If you are using context with timeout
 		if err := client.Disconnect(ctx); err != nil {
 			panic(err)
 		}
@@ -64,15 +70,43 @@ func main() {
 	coll := client.Database(dbName).Collection(dbCollectionName)
 
 	// CREATE OPERATION
-	newData := Data{Name: "Test New Data"}
-	result, err := coll.InsertOne(ctx, newData)
+	newData := Data{ID: primitive.NewObjectID(), Name: "Test New Data"}
+	createResult, err := coll.InsertOne(ctx, newData)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Inserted Data", result)
+	fmt.Println("Inserted Data :", createResult)
 
 	// READ OPERATION
+	findData := Data{}
+	filter := bson.D{{Key: "_id", Value: createResult.InsertedID}}
+	err = coll.FindOne(context.TODO(), filter).Decode(&findData)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			// This error means your query did not match any documents.
+			fmt.Println("Find Data :", "Not Found")
+		} else {
+			panic(err)
+		}
+	} else {
+		fmt.Println("Find Data :", findData)
+	}
+
 	// UPDATE OPERATION
+	update := bson.D{{Key: "$set", Value: bson.D{{Key: "updated_field", Value: true}}}}
+	updateResult, err := coll.UpdateOne(ctx, filter, update)
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("Data Updated :", updateResult.ModifiedCount)
+	}
+
 	// DELETE OPERATION
+	deleteResult, err := coll.DeleteOne(ctx, filter)
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Printf("Documents deleted: %d\n", deleteResult.DeletedCount)
+	}
 
 }
